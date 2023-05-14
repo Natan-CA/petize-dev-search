e44b81498d5e04d1042ea5aead219c1fbc24bae6
Surigota <natan.castelhano@hotmail.com>
2023-05-13 23:05:30 -0300

    assets and user info card with repository list

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -14,10 +14,27 @@ import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
 import Avatar from "@mui/material/Avatar";
+import mail from "./../assets/email-mail.png";
+import followers from "./../assets/group-user.png";
+import heart from "./../assets/heart.png";
+import link from "./../assets/link.png";
+import office from "./../assets/office.png";
+import pin from "./../assets/pin-location.png";
+import twitter from "./../assets/twitter.png";
+import star from "./../assets/star.png";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Link from "@mui/material/Link";
+import moment from "moment";
+import localization from "moment/locale/pt-br";
+moment.locale("pt-br", localization);
 
-function Perfil(props) {
+function Perfil() {
     let { user } = useParams();
-    const [repositorio, setRepositorio] = useState([]);
+    const [userInfo, setUserInfo] = useState([]);
+    const [repos, setRepos] = useState([]);
     const [name, setName] = useState(user);
     const navigate = useNavigate();
 
@@ -28,14 +45,100 @@ function Perfil(props) {
 
     const fetchUser = () => {
         axios.get(`https://api.github.com/users/${user}`).then((response) => {
-            console.log(response.data);
-            setRepositorio(response.data);
+            setUserInfo(response.data);
         });
+    };
+
+    const fetchRepos = () => {
+        axios
+            .get(
+                `https://api.github.com/search/repositories?q=user:${user}+stars:>=0`
+            )
+            .then((response) => {
+                setRepos(response.data);
+            });
     };
 
     useEffect(() => {
         fetchUser();
+        fetchRepos();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    function ItemLista({ icon, alt, data, prefix, isLink }) {
+        if (userInfo.length === 0 || data === null || data === "") return null;
+        else {
+            prefix = prefix || "";
+            return (
+                <ListItem disableGutters>
+                    <ListItemIcon sx={{ minWidth: 0, marginRight: 1 }}>
+                        <img src={icon} alt={alt} />
+                    </ListItemIcon>
+                    <ListItemText>
+                        {isLink ? (
+                            <Link
+                                href={
+                                    prefix
+                                        ? `https://twitter.com/${data}`
+                                        : data
+                                }
+                            >
+                                {prefix + data}
+                            </Link>
+                        ) : (
+                            <span>{prefix + data}</span>
+                        )}
+                    </ListItemText>
+                </ListItem>
+            );
+        }
+    }
+
+    function RepoLista({ rep }) {
+        if (rep.length === 0) return null;
+        else {
+            const repositorios = rep.items;
+            return repositorios.map((r, index) => (
+                <Card key={index} sx={{ marginBottom: 2 }}>
+                    <CardContent
+                        sx={{ paddingY: 0, "&:last-child": { pb: 0 } }}
+                    >
+                        <List>
+                            <ListItem disableGutters>
+                                <Link
+                                    href={r.html_url}
+                                    underline="hover"
+                                    color="inherit"
+                                >
+                                    <Typography variant="h6">
+                                        {r.name}
+                                    </Typography>
+                                </Link>
+                            </ListItem>
+                            <ListItem disableGutters>
+                                <Typography variant="body2" color='#4A5568'>
+                                    {r.description}
+                                </Typography>
+                            </ListItem>
+                            <ListItem disableGutters>
+                                <ListItemIcon
+                                    sx={{ minWidth: 0, marginRight: 1 }}
+                                >
+                                    <img src={star} alt="star_icon" />
+                                </ListItemIcon>
+                                <ListItemText>
+                                    <Typography variant="body2" color='#4A5568'>
+                                        {r.stargazers_count} •{" "}
+                                        {moment(r.updated_at).fromNow()}
+                                    </Typography>
+                                </ListItemText>
+                            </ListItem>
+                        </List>
+                    </CardContent>
+                </Card>
+            ));
+        }
+    }
 
     return (
         <Container maxWidth="lg">
@@ -88,26 +191,65 @@ function Perfil(props) {
                             <CardHeader
                                 avatar={
                                     <Avatar
-                                        src={repositorio.avatar_url}
+                                        src={userInfo.avatar_url}
                                         aria-label="usuario"
                                         alt="usuario"
                                     ></Avatar>
                                 }
-                                title={repositorio.name}
-                                subheader={repositorio.twitter_username}
+                                title={userInfo.name}
+                                subheader={userInfo?.login}
                             />
-                            <CardContent>
+                            <CardContent sx={{ paddingTop: 1 }}>
                                 <Typography variant="body2">
-                                    {'"a benevolent smile"'}
+                                    {userInfo?.bio}
                                 </Typography>
+                                <List dense={true}>
+                                    <ItemLista
+                                        icon={followers}
+                                        data={userInfo.followers}
+                                        alt="followers_icon"
+                                    />
+                                    <ItemLista
+                                        icon={heart}
+                                        data={userInfo.following}
+                                        alt="following_icon"
+                                    />
+                                    <br />
+                                    <ItemLista
+                                        icon={office}
+                                        data={userInfo.company}
+                                        alt="office_icon"
+                                    />
+                                    <ItemLista
+                                        icon={pin}
+                                        data={userInfo.location}
+                                        alt="pin_location_icon"
+                                    />
+                                    <ItemLista
+                                        icon={mail}
+                                        data={userInfo.email}
+                                        alt="email_icon"
+                                    />
+                                    <ItemLista
+                                        icon={link}
+                                        data={userInfo.blog}
+                                        alt="link_icon"
+                                        isLink={true}
+                                    />
+                                    <ItemLista
+                                        icon={twitter}
+                                        data={userInfo.twitter_username}
+                                        prefix={"@"}
+                                        alt="twitter_icon"
+                                        isLink={true}
+                                    />
+                                </List>
                             </CardContent>
                         </Card>
                     </Grid>
                     {/* repositories card */}
                     <Grid item xs={9} sx={{ marginTop: 12 }}>
-                        <Card>
-                            <CardContent></CardContent>
-                        </Card>
+                        <RepoLista rep={repos} />
                     </Grid>
                 </Grid>
             </Box>
